d64ba9369e7a99f180ccd4464f09257280bf6da1
Whichhunter404 <schmidt.peti32@gmail.com>
2020-02-19 21:08:21 +0100

    put build folder to the root

---
+++ precache-manifest.e75c24a118869ef24cd02ff0e0073ca1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d15e98d6fead72ea1a23e51f0ad4e48",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c8aae630f0910bcd25b",
+    "url": "/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "c8aafdf3bd9f6e71f430",
+    "url": "/static/css/main.2378d7e4.chunk.css"
+  },
+  {
+    "revision": "0c8aae630f0910bcd25b",
+    "url": "/static/js/2.917f4559.chunk.js"
+  },
+  {
+    "revision": "c8aafdf3bd9f6e71f430",
+    "url": "/static/js/main.3674264f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
